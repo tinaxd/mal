9f3e27e079dc92c044866119036430e67d34b8a1
tinaxd <31384595+tinaxd@users.noreply.github.com>
2024-03-30 21:09:06 +0900

    self hosting

---
+++ impls/mygo/src/stepA_mal/printer.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+func readableString(s string) string {
+	s = strings.ReplaceAll(s, "\\", "\\\\")
+	s = strings.ReplaceAll(s, "\"", "\\\"")
+	s = strings.ReplaceAll(s, "\n", "\\n")
+	return s
+}
+
+func PrStr(v MalValue, readably bool) string {
+	if v == nil {
+		return "nil"
+	}
+
+	switch vv := v.(type) {
+	case MalSymbol:
+		return vv.Value
+	case MalInt:
+		return strconv.FormatInt(vv.Value, 10)
+	case MalBool:
+		if vv.Value {
+			return "true"
+		} else {
+			return "false"
+		}
+	case MalFunc:
+		return "#<function>"
+	case MalTcoFunc:
+		return "#<function>"
+	case MalString:
+		if vv.IsKeyword() {
+			return ":" + vv.AsKeyword()
+		}
+		if readably {
+			return "\"" + readableString(vv.Value) + "\""
+		} else {
+			return vv.Value
+		}
+	case MalList:
+		var str string
+
+		if vv.IsVector() {
+			str += "["
+		} else {
+			str += "("
+		}
+
+		for i, value := range vv.Values {
+			if i != 0 {
+				str += " "
+			}
+			str += PrStr(value, readably)
+		}
+
+		if vv.IsVector() {
+			str += "]"
+		} else {
+			str += ")"
+		}
+
+		return str
+	case *MalAtom:
+		var v MalValue
+		if vv.Ref != nil {
+			v = vv.Ref
+		}
+		return "(atom " + PrStr(v, readably) + ")"
+	case *MalMap:
+		str := "{"
+		for i, kv := range vv.Iter() {
+			if i != 0 {
+				str += " "
+			}
+			str += PrStr(kv.Key, readably) + " " + PrStr(kv.Value, readably)
+		}
+		str += "}"
+		return str
+	}
+
+	panic("unreachable")
+}
